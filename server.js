4d5804c9dffc81defe1c13dbc9281a3eda8dca78
Jacqueline Kolze <38360663+Jkolze1@users.noreply.github.com>
2018-09-06 15:42:04 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
+require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
-var app = express();
+var exphbs = require("express-handlebars");
 var passport = require("passport");
+var app = express();
+var db = require("./models");
+var PORT = process.env.PORT || 7070;
 
-// Port
-var PORT = process.env.PORT || 3000;
 
+// Middleware
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  require('./config/passport')(db, passport); // pass passport for configuration
+ 
+// Handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
 
-// Starting the server, syncing our models ------------------------------------/ 
+// Routes
+require("./routes/apiRoutes")(app, passport);
+require("./routes/htmlRoutes")(app, passport);
+
+var syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV !== "production") {
+  syncOptions.force = true;
+}
+
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
@@ -15,8 +43,6 @@ var PORT = process.env.PORT || 3000;
       PORT
     );
   });
-  app.get('/', function(req, res){
-    res.sendFile(__dirname+'/public/index.html'); // change the path to your index.html
 });
 
 module.exports = app;
